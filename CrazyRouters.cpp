97ee74ae3ca5fa4cd99608597fb6bccb2e8561a1
sumitdhyani <sumitdhyani1@gmail.com>
2024-02-23 16:07:39 +0530

    Fixed the stack overflow problem in bot the req-resp routers

--- CrazyRouters.cpp
+++ CrazyRouters.cpp
@@ -1,8 +1,11 @@
-#pragma once
+#include "CrazyRouters.hpp"
 #include <functional>
 #include <unordered_map>
 #include <unordered_set>
 #include <memory>
+#include <array>
+#include <iostream>
+#include <string>
 
 //Acts a light weight request-response mechanism
 //Making it templatized make is to be able to be used as a type safe mechanism'
@@ -11,197 +14,58 @@
 
 //Acts a light weight pub-sub mechanism
 //Making it templatized make is to be able to be used as a type safe mechanism
-template<class Key, class RegistrationId, class... Data>
-struct PubSubDataRouter
-{
-	typedef std::function<void(const Data&...)> DataCallback;
 
-	//Produce Data with the specified key
-	bool produce(const Key& key, const Data&... data)
-	{
-		bool retVal = false;
-		if (auto it = m_routingTable.find(key); it != m_routingTable.end()) {
-			retVal = true;
-			for (const auto& [registrationId, callback] : it->second) {
-				callback(data...);
-				retVal = true;
-			}
-		}
-
-		return retVal;
-	}
-
-
-	bool consume(const Key& key, const RegistrationId& registrationId, const DataCallback& callback)
-	{
-		bool retVal = true;
-		if (auto it = m_routingTable.find(key); it != m_routingTable.end())
-			if (auto itCallback = it->second.find(registrationId); itCallback == it->second.end()) {
-				it->second[registrationId] = callback;
-				m_regIdToKeys[registrationId].insert(key);
-			}
-			else
-				retVal = false;
-		else {
-			m_routingTable[key] = { {registrationId, callback} };
-			m_regIdToKeys[registrationId].insert(key);
-		}
-
-		return retVal;
-	}
-
-	bool consumeEveryThing(const RegistrationId& registrationId, const DataCallback& callback)
-	{
-		return true;
-	}
-
-	bool unregister(const Key& key, const RegistrationId& registrationId)
-	{
-		bool retVal = false;
-		if (auto it = m_routingTable.find(key); it != m_routingTable.end()) {
-			if (auto itCallback = it->second.find(registrationId); itCallback != it->second.end()) {
-				it->second.erase(itCallback);
-
-				auto itRegIdToKeys = m_regIdToKeys.find(registrationId);
-				auto& [regId, keys] = *itRegIdToKeys;
-				keys.erase(key);
-				if (keys.empty()) {
-					m_regIdToKeys.erase(itRegIdToKeys);
-				}
-				retVal = true;
-			}
-		}
-
-		return retVal;
-	}
-
-	bool unregisterAll(const RegistrationId& registrationId)
-	{
-		if (auto it = m_regIdToKeys.find(registrationId); it != m_regIdToKeys.end()) {
-			const auto& [regId, keys] = *it;
-			for (const auto& key : keys) {
-				unregister(key, registrationId);
-			}
-			m_regIdToKeys.erase(it);
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-private:
-	std::unordered_map<Key, std::unordered_map<RegistrationId, DataCallback>> m_routingTable;
-	std::unordered_map<RegistrationId, std::unordered_set<Key>> m_regIdToKeys;
-};
-
-template <class Key, class... Data>
-struct ProducerFunctions
-{
-	std::function<bool (const Key&, const Data&...)> produce;
-};
-
-template <class Key, class RegistrationId, class... Data>
-struct ConsumerFunctions
-{
-	typedef std::function<void(const Data&...)> DataCallback;
-	std::function<bool(const Key&, const RegistrationId&, const DataCallback&)> consume;
-	std::function<bool(const RegistrationId&, const DataCallback&)> consumeEveryThing;
-	std::function<bool(const Key&, const RegistrationId&)> unregister;
-	std::function<void(const RegistrationId&)> unregisterAll;
-};
-
-template<class ResponderId, class ReqId, class ReqData, class... Response>
-struct EndToEndReqRespRouter {
-
-	typedef std::function<void(const Response&...)> ResponseHandler;
-	typedef std::function<void(const ReqData&, const ResponseHandler&)> ReqListener;
-	typedef PubSubDataRouter<ResponderId, size_t, ReqData, ResponseHandler> ReqRouter;
-	typedef PubSubDataRouter<ReqId, size_t, Response...> RespRouter;
-
-	bool registerAsResponder(const ResponderId& responderId, const ReqListener& reqListener) {
-		return m_reqRouter.consume(responderId,
-									(size_t)this,
-				[this, reqListener](const ReqData& reqData,
-									const ResponseHandler& responseHandler) {
-					reqListener(reqData, responseHandler);
-				}
-		);
-	}
-
-	bool unregisterAsResponder(const ResponderId& responderId) {
-		return m_reqRouter.unregister(responderId, (size_t)this);
-	}
-
-	bool request(const ResponderId& responderId,
-				 const ReqId& reqId,
-				 const ReqData& reqData,
-				 const ResponseHandler& responseHandler) {
-		if( m_respRouter.consume(reqId, (size_t)this, [this, reqId, responseHandler](const Response&... response) {
-			responseHandler(response...);
-			})
-		) {
-			m_reqRouter.produce(responderId, reqData, [this, reqId](const Response&... response) {
-				m_respRouter.produce(reqId, response...);
-				m_respRouter.unregister(reqId, (size_t)this);
-			});
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	bool cancelRequest(const ReqId& reqId) {
-		return m_respRouter.unregister(reqId, (size_t)this);
-	}
-
-	//~EndToEndReqRespRouter() {
-	//	m_reqRouter.unregisterAll((size_t)this);
-	//	m_respRouter.unregisterAll((size_t)this);
-	//}
-
-	private:
-	ReqRouter m_reqRouter;
-	RespRouter m_respRouter;
-};
-//////////////////////////////////////////////////////////////////////////////////////////////////
-#include <iostream>
-#include <string>
-
-
-int main() {
-	auto responder = [](size_t num, const std::function<void(size_t, size_t)> callback) {
+void demoEndToEndReqRespRouter() {
+	auto responder = [](const size_t& num, const std::function<void(const size_t&, const size_t&)>& callback) {
 		callback(num*num, num*num*num);
 	};
 	//template<class ResponderId, class ReqId, class ReqData, class... Response>
-	EndToEndReqRespRouter<size_t, size_t, size_t, size_t, size_t> reqRespRouter;
-	reqRespRouter.registerAsResponder(1, responder);
-	reqRespRouter.request(1, 1, 2, [](size_t res1, size_t res2) {
-		std::cout<< "Results: " << res1 << ", " << res2 << std::endl; 
-	});
-
-	PubSubDataRouter<size_t, size_t, size_t, std::string> pubSubRouter;
-	auto callback = [](const size_t& data, const std::string& str) {
-		std::cout << "Received: " << data << ", " << str.c_str() << std::endl;
+	EndToEndReqRespRouter<size_t, size_t, size_t, std::hash<size_t>, std::hash<size_t>, size_t, size_t> reqRespRouter;
+	reqRespRouter.registerAsResponder(0, responder);
+	size_t reqId = 1;
+	std::function<void(size_t,size_t)> resultHandler = [&reqId, &reqRespRouter, &resultHandler](const size_t& res1, const size_t& res2) mutable {
+		std::cout << "ReqId: " << reqId << ", Results: " << res1 << ", " << res2 << std::endl;
+		if (reqId++ < 100) {
+			std::cout << "Sending ReqId: " << reqId << std::endl;
+			reqRespRouter.request(0, reqId, reqId, resultHandler);
+		}
 	};
-//
-	//pubSubRouter.consume(1, 1, callback);
-	//pubSubRouter.consume(2, 1, callback);
-//
-	//pubSubRouter.produce(1, 1, "A");
-	//pubSubRouter.produce(1, 2, "B");
-	//pubSubRouter.produce(2, 3, "C");
-	//pubSubRouter.produce(2, 4, "D");
-//
-	//pubSubRouter.unregister(2, 1);
-	//std::cout << "Unregistered key: 2" << std::endl;
-//
-//
-	//pubSubRouter.produce(1, 1, "A");
-	//pubSubRouter.produce(1, 2, "B");
-	//pubSubRouter.produce(2, 3, "C");
-	//pubSubRouter.produce(2, 4, "D");
-//
-//
-	//std::cout << "Just before end" << std::endl;
-    return 0;
+
+	reqRespRouter.request(0, reqId, reqId, resultHandler);
+}
+
+void demoGenericReqRespRouter() {
+	using ResponseHandler = std::function<void(const size_t&, const size_t&)>;
+	using RequestHandler = std::function<void(const size_t&, const ResponseHandler&)>;
+	RequestHandler responder1 = [](const size_t& num, const ResponseHandler& callback) {
+		callback(num*num, num*num*num);
+	};
+
+	RequestHandler responder2 = [](const size_t& num, const ResponseHandler& callback) {
+		callback(num*2, num*3);
+	};
+
+	std::array<RequestHandler, 2> responders = {responder1, responder2};
+	
+	//template<class ResponderId, class ReqType, class ReqId, class ReqData, class ReqTypeHasher, class ReqIdHasher, class... Response>
+	GenericReqRespRouter<size_t, size_t, size_t, size_t, std::hash<size_t>, std::hash<size_t>, size_t, size_t> reqRespRouter;
+	reqRespRouter.registerAsResponder(0, 0, responder1);
+	reqRespRouter.registerAsResponder(1, 1, responder2);
+	size_t reqId = 1;
+	std::function<void(size_t,size_t)> resultHandler = [&reqId, &reqRespRouter, &resultHandler](const size_t& res1, const size_t& res2) mutable {
+		std::cout << "ReqId: " << reqId << ", Results: " << res1 << ", " << res2 << std::endl;
+		if (reqId++ < 100) {
+			std::cout << "Sending ReqId: " << reqId << std::endl;
+			reqRespRouter.request(reqId, reqId%2, reqId, resultHandler);
+		}
+	};
+
+	reqRespRouter.request(reqId, reqId%2, reqId, resultHandler);
+}
+
+
+int main() {
+	demoEndToEndReqRespRouter();
+	demoGenericReqRespRouter();
+	return 0;
 }
--- CrazyRouters.hpp
+++ CrazyRouters.hpp
@@ -4,43 +4,6 @@
 #include <unordered_set>
 #include <queue>
 
-template<class SubscriberId, class... Data>
-struct PubSubDataSink {
-	typedef std::function<void(Data...)> DataCallback;
-	bool produce(const Data&... data) {
-		if (m_routingTable.empty()) {
-			return false;
-		}
-
-		for (auto const& [subscriberId, callback] : m_routingTable) {
-			callback(data...);
-		}
-
-		return true;
-	}
-
-	bool consume(const SubscriberId& subscriberId, const DataCallback& callback) {
-		if (auto it = m_routingTable.find(subscriberId); it == m_routingTable.end()) {
-			m_routingTable[subscriberId] = callback;
-		} else {
-			return false;
-		}
-	}
-
-	bool unregister(const SubscriberId& subscriberId) {
-		bool retVal = false;
-		if (auto it = m_routingTable.find(subscriberId); it != m_routingTable.end()) {
-			m_routingTable.erase(it);
-			retVal = true;
-		}
-
-		return retVal;
-	}
-
-	private:
-	std::unordered_map<SubscriberId, DataCallback> m_routingTable;
-};
-
 //Acts a light weight pub-sub mechanism
 //Making it templatized make is to be able to be used as a type safe mechanism
 template<class Key, class SubscriberId, class KeyHasher, class... Data>
@@ -147,22 +110,6 @@ private:
 	std::unordered_map<SubscriberId, std::unordered_set<Key, KeyHasher>> m_regIdToKeys;
 };
 
-template <class Key, class... Data>
-struct ProducerFunctions
-{
-	std::function<bool (const Key&, const Data&...)> produce;
-};
-
-template <class Key, class SubscriberId, class... Data>
-struct ConsumerFunctions
-{
-	typedef std::function<void(const Data&...)> DataCallback;
-	std::function<bool(const Key&, const SubscriberId&, const DataCallback&)> consume;
-	std::function<bool(const SubscriberId&, const DataCallback&)> consumeEveryThing;
-	std::function<bool(const Key&, const SubscriberId&)> unregister;
-	std::function<bool(const SubscriberId&)> unregisterAll;
-};
-
 //Acts a light weight request-response mechanism
 //Making it templatized make is to be able to be used as a type safe mechanism'
 template<class ResponderId, class ReqId, class ReqData, class ResponderIdHasher, class ReqIdHasher, class... Response>
@@ -238,7 +185,6 @@ struct EndToEndReqRespRouter {
 	std::unordered_set<ReqId> m_pendingreqIds;
 };
 
-
 template<class ResponderId, class ReqType, class ReqId, class ReqData, class ReqTypeHasher, class ReqIdHasher, class... Response>
 struct GenericReqRespRouter {
 
@@ -246,6 +192,8 @@ struct GenericReqRespRouter {
 	typedef std::function<void(const ReqData&, const ResponseHandler&)> ReqListener;
 	typedef PubSubDataRouter<ReqType, size_t, ReqTypeHasher, ReqData, ResponseHandler> ReqRouter;
 	typedef PubSubDataRouter<ReqId, size_t, ReqIdHasher, Response...> RespRouter;
+
+	GenericReqRespRouter() : m_reqPending(false) {}
 
 	bool registerAsResponder(const ResponderId& responderId, const ReqType& reqType, const ReqListener& reqListener) {
 		if(auto it = m_responderBook.find(reqType); it == m_responderBook.end()) {
@@ -276,12 +224,28 @@ struct GenericReqRespRouter {
 		}
 
 		m_pendingreqIds.insert(reqId);
-		m_reqRouter.produce(reqType, reqData, [this, reqId, responseHandler](const Response&... response) {
-			if (auto it = m_pendingreqIds.find(reqId); it != m_pendingreqIds.end()) {
-				responseHandler(response...);
-				m_pendingreqIds.erase(it);
+		auto reqForwarder =
+		[this, reqId, reqType, reqData, responseHandler](){
+			m_reqPending = true;
+			m_reqRouter.produce(reqType, reqData, [this, reqId, responseHandler](const Response&... response) {
+				if (auto it = m_pendingreqIds.find(reqId); it != m_pendingreqIds.end()) {
+					responseHandler(response...);
+					m_pendingreqIds.erase(it);
+				}
+			});
+			m_reqPending = false;
+		};
+
+		if (m_reqPending) {
+			m_pendingReqProcessors.push(reqForwarder);
+		} else {
+			reqForwarder();
+			while (!m_pendingReqProcessors.empty()) {
+				m_pendingReqProcessors.front()();
+				m_pendingReqProcessors.pop();
 			}
-		});
+		}
+
 		return true;
 	}
 
@@ -296,149 +260,10 @@ struct GenericReqRespRouter {
 	}
 
 	private:
+	bool m_reqPending;
 	ReqRouter m_reqRouter;
+	std::queue<std::function<void()>> m_pendingReqProcessors;
 	std::unordered_set<ReqId> m_pendingreqIds;
 	std::unordered_map<ReqType, ResponderId> m_responderBook;
 };
-
-
-template<class ResponderId, class ReqId, class ReqData, class... Response>
-struct EndToEndResponderFunctions {
-	typedef std::function<void(const Response&...)> ResponseHandler;
-	typedef std::function<void(const ReqData&, const ResponseHandler&)> ReqListener;
-	std::function<bool(const ResponderId&, const ReqListener&)> registerAsResponder;
-	std::function<bool(const ResponderId&)> unregisterAsResponder;
-};
-
-template<class ResponderId, class ReqId, class ReqData, class... Response>
-struct EndToEndRequesterFunctions {
-	typedef std::function<void(const Response&...)> ResponseHandler;
-	std::function<bool(const ResponderId&,
-				 	   const ReqId&,
-				 	   const ReqData&,
-				 	   const ResponseHandler&)> request;
-
-	std::function<bool(const ReqId&)> cancelRequest;
-};
-
-template<class ResponderId, class ReqType, class ReqData, class... Response>
-struct GenericResponderFunctions {
-	typedef std::function<void(const Response&...)> ResponseHandler;
-	typedef std::function<void(const ReqData&, const ResponseHandler&)> ReqListener;
-
-	std::function<bool(const ResponderId&,
-					   const ReqType&,
-					   const ReqListener&)> registerAsResponder;
-
-	std::function<bool(const ResponderId&,
-					   const ReqType&)> unregisterAsResponder;
-};
-
-template<class ReqType, class ReqId, class ReqData, class... Response>
-struct GenericRequesterFunctions {
-	typedef std::function<void(const Response&...)> ResponseHandler;
-	std::function<bool (const ReqId& reqId,
-				  const ReqType& reqType,
-				  const ReqData& reqData,	
-				  const ResponseHandler& responseHandler)> request;
-	
-	std::function<bool(const ReqId& reqId)> cancelRequest;
-};
-
-
-template<class Key, class SubData, class UnsubData, class KeyHasher = std::hash<Key>, class KeyEquator = std::equal_to<Key>>
-struct SingleLinkSubUnsubHandler
-{
-	typedef std::function<void(const SubData&)> SubAction;
-	typedef std::function<void(const UnsubData&)> UnsubAction;
-	typedef std::function<UnsubData(const Key&)> UnsubDataGenerator;
-
-	bool subscribe(const Key& key,
-				   const SubData& subData,
-				   const SubAction& subAction)
-	{
-		if (m_subscriptionBook.insert(key).second)
-		{
-			subAction(subData);
-			return true;
-		}
-		else
-			return false;
-	}
-
-	bool unsubscribe(const Key& key,
-					 const UnsubData& unsubData,
-					 const UnsubAction& unsubAction)
-	{
-		if (m_subscriptionBook.erase(key))
-		{
-			unsubAction(unsubData);
-			return true;
-		}
-		else
-			return false;
-	}
-
-	void unsubscribeAll(const UnsubDataGenerator&  unsubDataGenerator, 
-						const UnsubAction& unsubAction) {
-		for (auto const& key : m_subscriptionBook) {
-			unsubAction(unsubDataGenerator(key));
-		}
-
-		m_subscriptionBook.clear();
-	}
-
-private:
-	std::unordered_set<Key, KeyHasher, KeyEquator> m_subscriptionBook;
-};
-
-template<class Key, class SubData, class UnsubData, class KeyHasher = std::hash<Key>, class KeyEquator = std::equal_to<Key>>
-struct AgregatedSubUnsubHandler
-{
-	typedef std::function<void(const SubData&)> SubAction;
-	typedef std::function<void(const UnsubData&)> UnsubAction;
-
-	bool subscribe(const Key& key, const SubData& data, const SubAction& subAction)
-	{
-		if (auto it = m_subscriptionBook.find(key); it != m_subscriptionBook.end())
-		{
-			++it->second;
-			return false;
-		}
-		else
-		{
-			m_subscriptionBook[key] = 1;
-			subAction(data);
-			return true;
-		}
-	}
-
-	bool unsubscribe(const Key& key, const UnsubData& data, const UnsubAction& unsubAction)
-	{
-		if (auto it = m_subscriptionBook.find(key); it != m_subscriptionBook.end())
-		{
-			if (!(--it->second))
-			{
-				unsubAction(data);
-				m_subscriptionBook.erase(it);
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	void resubscribeAll(const std::function<SubData(Key)>& subDataGenerator, const SubAction& subAction)
-	{
-		for (auto const& [key, numSubscribers] : m_subscriptionBook) {
-			subAction(subDataGenerator(key));
-		}
-	}
-private:
-	std::unordered_map<Key,
-					   size_t,
-					   KeyHasher,
-					   KeyEquator> m_subscriptionBook;
-};
-
 
