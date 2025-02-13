e94bc3166d46d9873bbccacb0b006f5103ac6230
sumitdhyani <sumitdhyani1@gmail.com>
2025-02-13 15:34:21 +0530

    reverse_iterator implementation

--- list.cpp
+++ list.cpp
@@ -59,7 +59,8 @@ struct list
 
     const_iterator operator ++()
     {
-      return m_node = m_node->m_next;
+      m_node = m_node->m_next;
+      return *this;
     }
 
     const T* operator->()
@@ -81,10 +82,68 @@ struct list
     Node* m_node;
   };
 
+  struct reverse_iterator
+  {
+    reverse_iterator(Node* node) : m_node(node)
+    {}
+
+    reverse_iterator operator ++()
+    {
+      m_node = m_node->m_prev;
+      return *this;
+    }
+
+    T* operator->()
+    {
+      return &m_node->m_val;
+    }
+
+    T& operator*()
+    {
+      return m_node->m_val;
+    }
+
+    bool operator!=(const reverse_iterator& other)
+    {
+      return m_node != other.m_node;
+    }
+
+    private:
+    Node* m_node;
+  };
+
+  struct const_reverse_iterator
+  {
+    const_reverse_iterator(Node* node) : m_node(node)
+    {}
+
+    const_reverse_iterator operator ++()
+    {
+      m_node = m_node->m_prev;
+      return *this;
+    }
+
+    const T* operator->()
+    {
+      return &m_node->m_val;
+    }
+
+    const T& operator*()
+    {
+      return m_node->m_val;
+    }
+
+    bool operator!=(const const_reverse_iterator& other)
+    {
+      return m_node != other.m_node;
+    }
+
+    private:
+    Node* m_node;
+  };
+
   list() : m_start(nullptr), m_end(nullptr), m_size(0)
   {}
-
-  
 
   iterator begin()
   {
@@ -115,6 +174,38 @@ struct list
   const_iterator end() const
   {
     static const_iterator end(nullptr);
+    return end;
+  }
+
+  reverse_iterator rbegin()
+  {
+    if (m_size)
+    {
+      return reverse_iterator(m_end);
+    }
+
+    return rend();
+  }
+
+  reverse_iterator rend()
+  {
+    static reverse_iterator end(nullptr);
+    return end;
+  }
+
+  const_reverse_iterator rbegin() const
+  {
+    if (m_size)
+    {
+      return const_reverse_iterator(m_end);
+    }
+
+    return end();
+  }
+
+  const_reverse_iterator rend() const
+  {
+    static const_reverse_iterator end(nullptr);
     return end;
   }
 
@@ -180,6 +271,90 @@ struct list
     ++m_size;
   }
 
+  void pop_back()
+  {
+    if (!m_size)
+    {
+      return;
+    }
+
+    cleanupTillEnd(m_end);
+    --m_size;
+  }
+
+  void pop_front()
+  {
+    if (!m_size)
+    {
+      return;
+    }
+
+    cleanupTillStart(m_start);
+    --m_size;
+  }
+
+  iterator erase(iterator pos)
+  {
+    Node* node = pos.m_node;
+    Node* prev = node->m_prev;
+    Node* next = node->m_next;
+
+    pos++;
+
+    if (prev)
+    {
+      prev->m_next = next;
+    }
+
+    if(next)
+    {
+      next->m_prev = prev;
+    }
+
+    cleanupTillEnd(node, node);
+    return pos;
+  }
+
+  const_iterator erase(const_iterator pos)
+  {
+    iterator res = erase(iterator(pos.m_node));
+    return const_iterator(res.m_node);
+  }
+
+  iterator erase(iterator start, iterator end)
+  {
+    cleanupTillEnd(start.m_node, end != end()? end.m_node->m_prev : (Node*)nullptr);
+    return end;
+  }
+
+  const_iterator erase(const_iterator start, const_iterator end)
+  {
+    erase(iterator(start.m_node), iterator(end.m_node));
+    return end;
+  }
+
+  reverse_iterator erase(reverse_iterator end, reverse_iterator start)
+  {
+    cleanupTillStart(end.m_node, start != rend()? start.m_node->m_next : (Node*)nullptr);
+    return end;
+  }
+
+  const_reverse_iterator erase(const_reverse_iterator end, const_reverse_iterator start)
+  {
+    erase(reverse_iterator(end.m_node), reverse_iterator(start.m_node));
+    return end;
+  }
+
+  bool empty()
+  {
+    return m_size == 0;
+  }
+
+  uint32_t size()
+  {
+    return m_size;
+  }
+
   ~list()
   {
     cleanupTillEnd(m_start);
@@ -193,6 +368,49 @@ struct list
     {
       cleanupTillEnd(start->m_next);
       delete start;
+    }
+  }
+
+  void cleanupTillStart(Node* end)
+  {
+    if (end)
+    {
+      cleanupTillEnd(end->m_prev);
+      delete end;
+    }
+  }
+
+  void cleanupTillEnd(Node* start, Node* end)
+  {
+    if (!end)
+    {
+      cleanupTillEnd(start);
+    }
+    else
+    {
+      if (start != end)
+      {
+        cleanupTillEnd(start->m_next, end);
+      }
+
+      delete start;
+    }
+  }
+
+  void cleanupTillStart(Node* end, Node* start)
+  {
+    if (!start)
+    {
+      cleanupTillStart(end);
+    }
+    else
+    {
+      if (end != start)
+      {
+        cleanupTillStart(end->m_prev, start);
+      }
+
+      delete end;
     }
   }
   
@@ -251,7 +469,12 @@ int main()
     l.emplace_back(i);
   }
 
-  for (auto it = l.begin(); it != l.end(); ++it)
+  for (auto it = l.rbegin(); it != l.rend(); ++it)
+  {
+    std::cout << *it << std::endl;
+  }
+
+  for (auto it = l.rbegin(); it != l.rend(); ++it)
   {
     std::cout << *it << std::endl;
   }
